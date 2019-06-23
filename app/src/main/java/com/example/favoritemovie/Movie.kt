19406e8ec304c5c6228dd559efdfe5f0770dd0ea
cloudymous <sayacloud@gmail.com>
2019-06-23 15:52:55 +0800

    Delete some unused variable

--- app/src/main/java/com/example/favoritemovie/Movie.kt
+++ app/src/main/java/com/example/favoritemovie/Movie.kt
@@ -3,7 +3,6 @@ package com.example.favoritemovie
 class Movie {
     var movieName: String? = null
     var movieRating: String? = null
-    var movieReleaseDate: String? = null
     var movieOverview: String? = null
     var moviePoster: String? = null
 }
