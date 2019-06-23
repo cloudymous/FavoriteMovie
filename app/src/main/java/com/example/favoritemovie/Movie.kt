be4ff1bd2ba7a2dd252901c3c336c095670297e7
cloudymous <sayacloud@gmail.com>
2019-06-23 15:19:47 +0800

    Create Movie Class

---
+++ app/src/main/java/com/example/favoritemovie/Movie.kt
@@ -0,0 +1,9 @@
+package com.example.favoritemovie
+
+class Movie {
+    var movieName: String? = null
+    var movieRating: String? = null
+    var movieReleaseDate: String? = null
+    var movieOverview: String? = null
+    var moviePoster: String? = null
+}
