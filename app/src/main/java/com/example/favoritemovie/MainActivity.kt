633b90b0771bb3f15dffd314d181e088a9ae8f37
cloudymous <sayacloud@gmail.com>
2019-06-24 15:16:06 +0800

    Complete main Activity

--- app/src/main/java/com/example/favoritemovie/MainActivity.kt
+++ app/src/main/java/com/example/favoritemovie/MainActivity.kt
@@ -1,12 +1,31 @@
+
 package com.example.favoritemovie
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var rvList: RecyclerView
+    private var list: ArrayList<Movie> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvList = findViewById(R.id.recyclerMainMenu)
+        rvList.setHasFixedSize(true)
+
+        list.addAll(MovieData.listData)
+        showRecylerList()
+
+    }
+
+    private fun showRecylerList() {
+        rvList.layoutManager = LinearLayoutManager(this)
+        val listMovieAdapter = ListMovieAdapter(list)
+        rvList.adapter = listMovieAdapter
     }
 }
