b6c73a11595bbcc2914c84e74d72f5e0469cf086
cloudymous <sayacloud@gmail.com>
2019-06-26 12:06:34 +0800

    Complete movie data.

--- app/src/main/java/com/example/favoritemovie/MovieData.kt
+++ app/src/main/java/com/example/favoritemovie/MovieData.kt
@@ -14,6 +14,116 @@ object MovieData {
             "6.3",
             "The X-Men face their most formidable and powerful foe when one of their own, Jean Grey, starts to spiral out of control. During a rescue mission in outer space, Jean is nearly killed when she's hit by a mysterious cosmic force. Once she returns home, this force not only makes her infinitely more powerful, but far more unstable. The X-Men must now band together to save her soul and battle aliens that want to use Grey's new abilities to rule the galaxy.",
             "https://image.tmdb.org/t/p/w500/kZv92eTc0Gg3mKxqjjDAM73z9cy.jpg"
+        ),
+        arrayOf(
+            "Captain Marvel",
+            "7",
+            "The story follows Carol Danvers as she becomes one of the universe’s most powerful heroes when Earth is caught in the middle of a galactic war between two alien races. Set in the 1990s, Captain Marvel is an all-new adventure from a previously unseen period in the history of the Marvel Cinematic Universe.",
+            "https://image.tmdb.org/t/p/w500/AtsgWhDnHTq68L0lLsUrCnM7TjG.jpg"
+        ),
+        arrayOf(
+            "Aladdin",
+            "7.2",
+            "A kindhearted street urchin named Aladdin embarks on a magical adventure after finding a lamp that releases a wisecracking genie while a power-hungry Grand Vizier vies for the same lamp that has the power to make their deepest wishes come true.",
+            "https://image.tmdb.org/t/p/w500/3iYQTLGoy7QnjcUYRJy4YrAgGvp.jpg"
+        ),
+        arrayOf(
+            "Child's Play",
+            "5.8",
+            "Karen, a single mother, gifts her son Andy a Buddi doll for his birthday, unaware of its more sinister nature. A contemporary re-imagining of the 1988 horror classic.",
+            "https://image.tmdb.org/t/p/w500/rpS7ROczWulqfaXG2klYapULXKm.jpg"
+
+        ),
+        arrayOf(
+            "John Wick: Chapter 3 – Parabellum",
+            "7.2",
+            "Super-assassin John Wick returns with a $14 million price tag on his head and an army of bounty-hunting killers on his trail. After killing a member of the shadowy international assassin’s guild, the High Table, John Wick is excommunicado, but the world’s most ruthless hit men and women await his every turn.",
+            "https://image.tmdb.org/t/p/w500/ziEuG1essDuWuC5lpWUaw1uXY2O.jpg"
+        ),
+        arrayOf(
+            "Re: Zero kara Hajimeru Isekai Seikatsu - Memory Snow",
+            "5.4",
+            "Subaru and friends finally get a moment of peace, and Subaru goes on a certain secret mission that he must not let anyone find out about! However, even though Subaru is wearing a disguise, Petra and other children of the village immediately figure out who he is. Now that his mission was exposed within five seconds of it starting, what will happen with Subaru's \"date course\" with Emilia?",
+            "https://image.tmdb.org/t/p/w500/xqR4ABkFTFYe8NDJi3knwWX7zfn.jpg"
+
+        ),
+        arrayOf(
+            "Avengers: Infinity War",
+            "8.3",
+            "As the Avengers and their allies have continued to protect the world from threats too large for any one hero to handle, a new danger has emerged from the cosmic shadows: Thanos. A despot of intergalactic infamy, his goal is to collect all six Infinity Stones, artifacts of unimaginable power, and use them to inflict his twisted will on all of reality. Everything the Avengers have fought for has led up to this moment - the fate of Earth and existence itself has never been more uncertain.",
+            "https://image.tmdb.org/t/p/w500/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg"
+        ),
+        arrayOf(
+            "Spider-Man: Far from Home",
+            "6",
+            "Peter Parker and his friends go on a summer trip to Europe. However, they will hardly be able to rest - Peter will have to agree to help Nick Fury uncover the mystery of creatures that cause natural disasters and destruction throughout the continent.",
+            "https://image.tmdb.org/t/p/w500/dzg8lHS78l5Xxpd2EM5H541Qn2s.jpg"
+        ),
+        arrayOf(
+            "Escape Plan: The Extractors",
+            "5.5",
+            "After security expert Ray Breslin is hired to rescue the kidnapped daughter of a Hong Kong tech mogul from a formidable Latvian prison, Breslin's girlfriend is also captured. Now he and his team must pull off a deadly rescue mission to confront their sadistic foe and save the hostages before time runs out.",
+            "https://image.tmdb.org/t/p/w500/mgHu3OcwEFAm3GV9p2AoCoCEJPP.jpg"
+        ),
+        arrayOf(
+            "Catarina and the others",
+            "4.4",
+            "Outside, the first sun rays break the dawn.  Sixteen years old Catarina can't fall asleep.  Inconsequently, in the big city adults are moved by desire...  Catarina found she is HIV positive. She wants to drag everyone else along.",
+            "https://image.tmdb.org/t/p/w500/kZMCbp0o46Tsg43omSHNHJKNTx9.jpg"
+        ),
+        arrayOf(
+            "Avengers: Endgame",
+            "8.4",
+            "After the devastating events of Avengers: Infinity War, the universe is in ruins due to the efforts of the Mad Titan, Thanos. With the help of remaining allies, the Avengers must assemble once more in order to undo Thanos' actions and restore order to the universe once and for all, no matter what consequences may be in store.",
+            "https://image.tmdb.org/t/p/w500/or06FN3Dka5tukK1e9sl16pB3iy.jpg"
+        ),
+        arrayOf(
+            "Annabelle Comes Home",
+            "5",
+            "Determined to keep Annabelle from wreaking more havoc, demonologists Ed and Lorraine Warren bring the possessed doll to the locked artifacts room in their home, placing her “safely” behind sacred glass and enlisting a priest’s holy blessing. But an unholy night of horror awaits as Annabelle awakens the evil spirits in the room, who all set their sights on a new target—the Warrens' ten-year-old daughter, Judy, and her friends.",
+            "https://image.tmdb.org/t/p/w500/jsbt03UnPPt14AYRMcGaoqtwNxQ.jpg"
+        ),
+        arrayOf(
+            "Dumbo",
+            "6.5",
+            "A young elephant, whose oversized ears enable him to fly, helps save a struggling circus, but when the circus plans a new venture, Dumbo and his friends discover dark secrets beneath its shiny veneer.",
+            "https://image.tmdb.org/t/p/w500/279PwJAcelI4VuBtdzrZASqDPQr.jpg"
+        ),
+        arrayOf(
+            "How to Train Your Dragon: The Hidden World",
+            "7.6",
+            "As Hiccup fulfills his dream of creating a peaceful dragon utopia, Toothless’ discovery of an untamed, elusive mate draws the Night Fury away. When danger mounts at home and Hiccup’s reign as village chief is tested, both dragon and rider must make impossible decisions to save their kind.",
+            "https://image.tmdb.org/t/p/w500/xvx4Yhf0DVH8G4LzNISpMfFBDy2.jpg"
+        ),
+        arrayOf(
+            "Murder Mystery",
+            "6.5",
+            "After attending a gathering on a billionaire's yacht during a European vacation, a New York cop and his wife become prime suspects when he's murdered.",
+            "https://image.tmdb.org/t/p/w500/bSMSO9xupd4R4vwTPqigHn2quLN.jpg"
+        ),
+        arrayOf(
+            "Detective Conan: The Fist of Blue Sapphire",
+            "4.8",
+            "23rd movie in the \"Detective Conan\" franchise.",
+            "https://image.tmdb.org/t/p/w500/86Y6qM8zTn3PFVfCm9J98Ph7JEB.jpg"
+        ),
+        arrayOf(
+            "Godzilla: King of the Monsters",
+            "6.3",
+            "The new story follows the heroic efforts of the crypto-zoological agency Monarch as its members face off against a battery of god-sized monsters, including the mighty Godzilla, who collides with Mothra, Rodan, and his ultimate nemesis, the three-headed King Ghidorah. When these ancient super-species—thought to be mere myths—rise again, they all vie for supremacy, leaving humanity’s very existence hanging in the balance.",
+            "https://image.tmdb.org/t/p/w500/pU3bnutJU91u3b4IeRPQTOP8jhV.jpg"
+        ),
+        arrayOf(
+            "Alita: Battle Angel",
+            "6.7",
+            "When Alita awakens with no memory of who she is in a future world she does not recognize, she is taken in by Ido, a compassionate doctor who realizes that somewhere in this abandoned cyborg shell is the heart and soul of a young woman with an extraordinary past.",
+            "https://image.tmdb.org/t/p/w500/xRWht48C2V8XNfzvPehyClOvDni.jpg"
+        ),
+        arrayOf(
+            "Cars",
+            "6.7",
+            "Lightning McQueen, a hotshot rookie race car driven to succeed, discovers that life is about the journey, not the finish line, when he finds himself unexpectedly detoured in the sleepy Route 66 town of Radiator Springs. On route across the country to the big Piston Cup Championship in California to compete against two seasoned pros, McQueen gets to know the town's offbeat characters.",
+            "https://image.tmdb.org/t/p/w500/jpfkzbIXgKZqCZAkEkFH2VYF63s.jpg"
         )
 
     )
